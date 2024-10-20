3111974d9deeb23de7c677271cf8ad39592cf6c4
FarrukhTahir5 <farrukhtahir5@gmail.com>
2024-10-20 15:10:24 +0500

    hash router

--- React-project/src/main.jsx
+++ React-project/src/main.jsx
@@ -1,50 +1,49 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import {createBrowserRouter} from 'react-router-dom'
-import Home from './Pages/Home.jsx'
-import About from './Pages/About.jsx'
-import Courses from './Pages/Courses.jsx'
-import Contact from './Pages/Contact.jsx'
-import Nopage from './Pages/Nopage.jsx'
-import Signup from './Pages/Signup.jsx'
-import SignIn from './Pages/SignIn.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom'; // Use HashRouter
+import Home from './Pages/Home.jsx';
+import About from './Pages/About.jsx';
+import Courses from './Pages/Courses.jsx';
+import Contact from './Pages/Contact.jsx';
+import Nopage from './Pages/Nopage.jsx';
+import Signup from './Pages/Signup.jsx';
+import SignIn from './Pages/SignIn.jsx';
 
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/courses',
+    element: <Courses />,
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/signIn',
+    element: <SignIn />,
+  },
+  {
+    path: '*',
+    element: <Nopage />,
+  },
+]);
 
-const router =createBrowserRouter([
-  {
-    path:'/',
-    element:<Home/>
-  },
-  {
-    path:'/about',
-    element:<About/>
-  },
-  {
-    path:'/courses',
-    element:<Courses/>
-  },
-  {
-    path:'/contact',
-    element:<Contact/>
-  },
-  {
-    path:'/signup',
-    element:<Signup/>
-  },
-  {
-    path:'/signIn',
-    element:<SignIn/>
-  },
-  {
-    path:'*',
-    element:<Nopage/>
-  },
-])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
